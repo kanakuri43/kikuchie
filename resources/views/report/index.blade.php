253332bbcc63050de266c39502e249beed0ab960
okita <t-okita@netoffice.obisan.co.jp>
2022-09-09 14:49:57 +0900

    年月選択欄 追加 今月を初期値

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -10,7 +10,20 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+        window.onload = function() {
+            var today = new Date();
+            today.setDate(today.getDate());
+            var yyyy = today.getFullYear();
+            var mm = ("0" + (today.getMonth() + 1)).slice(-2);
+            var dd = ("0" + today.getDate()).slice(-2);
+            document.getElementById("month").value = yyyy + '-' + mm ;
+        }
+    </script>
 </head>
+
+
+
 
 <body>
     <div class="container">
@@ -18,6 +31,7 @@
 
 
             <h1>日報一覧</h1>
+            <p><input type="month" class="form-control" id="month"></p>
             <p><a href="{{ route('report.create') }}" class="text-decoration-none">新規追加</a></p>
 
             @if ($message = Session::get('success'))
