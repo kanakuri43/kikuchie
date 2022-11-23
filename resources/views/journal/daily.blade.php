1e0df4570a9e7f6906593cac2ef31a2708ba1652
okita <junor01@gmail.com>
2022-11-23 15:57:52 +0900

    アイコン表示

--- resources/views/journal/daily.blade.php
+++ resources/views/journal/daily.blade.php
@@ -18,7 +18,7 @@
 
 
             <h1>{{ $operation_date }} 日報</h1>
-            <p><a href=" {{ route('journal.monthly', date('Y-m')) }}" class="text-decoration-none">一覧画面</a></p>
+            <p><a href=" {{ route('journal.monthly', date('Y-m')) }}" class="text-decoration-none"><i class="bi bi-house-door-fill"></i> 一覧画面</a></p>
 
             @if ($message = Session::get('success'))
             <p>{{ $message }}</p>
--- resources/views/journal/monthly.blade.php
+++ resources/views/journal/monthly.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script type="text/javascript">
         window.onload = function() {
@@ -24,10 +25,11 @@
 <body>
     <div class="container">
         <div class="mx-auto" style="width:400px;">
+            
 
             <h1>日報一覧</h1>
             <p><input type="month" class="form-control" id="month" onChange="ex()"></p>
-            <p><a href="{{ route('journal.create') }}" class="text-decoration-none">新規追加</a></p>
+            <p><a href="{{ route('journal.create') }}" class="text-decoration-none"><i class="bi bi-journal-plus"></i> 新規追加</a></p>
 
             @if ($message = Session::get('success'))
             <p>{{ $message }}</p>
